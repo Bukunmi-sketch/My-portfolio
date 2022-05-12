ec74f6412ae48a112a4797ebad44b47e18fc8339
Bukunmi Olarinde <Olarindebukunmi@gmail.com>
2022-05-12 16:22:04 -0700

    All files uploaded
    upload all files

---
+++ b.js
@@ -0,0 +1,16 @@
+let barButton=document.querySelector('.bars');
+let headerUl=document.querySelector('header ul');
+let closebar=document.querySelector(".closebar");
+
+barButton.onclick=()=>{
+    headerUl.style.left="0";
+    closebar.style.display="block";
+    barButton.style.display="none";
+}
+
+closebar.onclick=()=>{
+    headerUl.style.left="-100%";
+    closebar.style.display="none";
+    barButton.style.display="block";
+}
+
